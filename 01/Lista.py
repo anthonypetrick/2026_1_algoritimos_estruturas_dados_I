dad7ecb3b36bc4543754b8926586f56998e106f9
04203990033 <04203990033@senacrs.edu.br>
2026-03-16 21:32:33 -0300

    lsita encadeada - continuação

--- 01/Lista.py
+++ 01/Lista.py
@@ -1,24 +1,23 @@
 from No import No
-
 class Lista:
 
     def __init__(self):
         self.inicio = None
 
-    def add(self, valor)
+    def add(self, valor):
         nodo = No(valor)
         if self.inicio is None:
             self.inicio = nodo
         else:
-            if nodo.dado < self.inicio:
-                nodo.proximo = self.inicio
+            if nodo.dado < self.inicio.dado:
+                nodo.prox = self.inicio
                 self.inicio = nodo
             else:
                 ant = self.inicio
                 aux = self.inicio.prox
 
                 while aux :
-                    if nodo.dado < aux.dado
+                    if nodo.dado < aux.dado:
                         nodo.prox = aux
                         ant.prox = nodo
                         break
@@ -30,11 +29,41 @@ class Lista:
         self.imprimir()
 
 
-    def print(self):
-        print("\nLista Encadeada")
-        if self.inicio = None
+
+
+    def imprimir(self):
+        print("\n----------------------")
+        print("Lista Encadeada")
+
+        if self.inicio is None:
             print("\nLista Vazia")
+            return
         aux = self.inicio
         while aux :
-            print("\n" + aux.dado)
+            print(  aux.dado )
             aux = aux.prox
+
+    def remover (sel, valor):
+        removeu = False
+        if self.inicio == None:
+            print("Lilsta Vazia")
+        else:
+            if valor == self.inicio.dado:
+                self.inicio = self.inicio.prox
+                removeu = True
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux:
+                    if valor == aux.dado:
+                        ant.prox = aux.prox
+                        removeu = True
+                        break
+                    else:
+                        ant = aux
+                        aux = aux.prox
+            if removeu:
+                print ("\n," valor,"removido")
+            else:
+                print ("\n," valor, "não encontrado na lista")
+            self.imprimir()
---
+++ 01/main.py
@@ -0,0 +1,16 @@
+
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+
+lista.add( "João" )
+lista.add( "Antônio" )
+lista.add( "Gabriel")
+lista.add( "Antoni" )
+lista.add( "Lorenzo" )
+
+lista.remover ("Gabriel")
+lista.remover ("Antoni")
+lista.remover ("Adalto")
